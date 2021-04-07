a0eeba94d2ac44814f703a54956585be4b5b6391
diegocb81 <diegocb81@gmail.com>
2021-04-07 15:53:01 -0300

    Desenvolvendo opcaoSelecionada

--- app/src/main/java/com/cursoandroid/projeto_pedra_papel_tesoura_app/MainActivity.java
+++ app/src/main/java/com/cursoandroid/projeto_pedra_papel_tesoura_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.cursoandroid.projeto_pedra_papel_tesoura_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionadoPedra(View view){
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionadoPapel(View view){
+        this.opcaoSelecionada("papel");
+    }
+
+    public void selecionadoTesoura(View view){
+        this.opcaoSelecionada("tesoura");
+    }
+
+
+
+    public void opcaoSelecionada(String opcaoSelecionada){
+        System.out.println("item clicado: " + opcaoSelecionada);
     }
 }
